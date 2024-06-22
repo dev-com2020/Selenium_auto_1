6e69c9e13ed00b19cdcea627d9b66163df0da4b3
DEV-COM <kwp@tlen.pl>
2024-06-22 14:29:50 +0200

    dodanie @pytest

--- start.py
+++ start.py
@@ -1,11 +1,15 @@
 from test_tc1 import TestTc1
+import pytest
 
-test1 = TestTc1()
-test1.setup_method(method=None, browser='chrome', headless=True)
-test1.test_tc1()
-test1.teardown_method(method=None)
 
-test2 = TestTc1()
-test2.setup_method(method=None, browser='firefox', headless=True)
-test2.test_tc1()
-test2.teardown_method(method=None)
+@pytest.mark.parametrize("browser, headless", [
+    ('chrome', True),
+    ('firefox', True)
+])
+def test_runner(browser, headless):
+    test = TestTc1()
+    test.setup_method(method=None, browser=browser, headless=headless)
+    try:
+        test.test_tc1()
+    finally:
+        test.teardown_method(method=None)
