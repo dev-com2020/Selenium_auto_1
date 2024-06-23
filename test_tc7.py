fca59451faad8422feb7e46cc68d6a5ce6d50f72
DEV-COM <kwp@tlen.pl>
2024-06-23 15:32:55 +0200

    actions

--- test_tc7.py
+++ test_tc7.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-class TestTc6:
+class TestTc7:
 
     @pytest.fixture(scope='class')
     def driver(self):
